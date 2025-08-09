546218894bbbd696b79c19b60eba7060c03b75d2
Jiawei Zhao <phoenix500526@163.com>
2025-08-09 16:00:04 +0000

    selftests/bpf: Add a test case in usdt_o2 to cover RIP handling logic
    When using GCC on x86-64 to compile a usdt prog with -O1 or higher
    optimization, the compiler will generate SIB addressing mode for global
    array and PC-relative addressing mode for global variable,
    e.g. "8@t1(%rip)" and "8@4+t1(%rip)".
    
    In this patch:
    - add usdt_rip test case to cover PC-relative addressing usdt argument spec
      handling logic
    
    Signed-off-by: Jiawei Zhao <phoenix500526@163.com>

--- tools/testing/selftests/bpf/prog_tests/usdt_o2.c
+++ tools/testing/selftests/bpf/prog_tests/usdt_o2.c
@@ -11,20 +11,39 @@ int lets_test_this(int);
 #define test_value 0xFEDCBA9876543210ULL
 #define SEC(name) __attribute__((section(name), used))
 
-
 static volatile __u64 array[1] = {test_value};
-unsigned short test_usdt1_semaphore SEC(".probes");
+unsigned short test_usdt_sib_semaphore SEC(".probes");
+unsigned short test_usdt_rip_semaphore SEC(".probes");
+
+static volatile struct st{
+	char a;
+	long b;
+};
+
+static volatile struct st ti = {.a = 'a', .b = 1024};
+static volatile char c = 'b';
+
+int add(int a, int b) {
+  return a + b;
+}
+
+int (*add_ptr)(int, int) = add;
 
 static __always_inline void trigger_func(void)
 {
 	/* Base address + offset + (index * scale) */
-	if (test_usdt1_semaphore) {
+	if (test_usdt_sib_semaphore) {
 		for (volatile int i = 0; i <= 0; i++)
-			STAP_PROBE1(test, usdt1, array[i]);
+			STAP_PROBE1(test, usdt_sib, array[i]);
 	}
+
+	if (test_usdt_rip_semaphore) {
+		STAP_PROBE3(test, usdt_rip, c, ti.b, add_ptr);
+	}
+
 }
 
-static void basic_sib_usdt(void)
+static void basic_usdt(void)
 {
 	LIBBPF_OPTS(bpf_usdt_opts, opts);
 	struct test_usdt_o2 *skel;
@@ -42,22 +61,36 @@ static void basic_sib_usdt(void)
 	if (!ASSERT_OK(err, "skel_attach"))
 		goto cleanup;
 
-	/* usdt1 won't be auto-attached */
+	/* usdt_sib won't be auto-attached */
 	opts.usdt_cookie = 0xcafedeadbeeffeed;
-	skel->links.usdt1 = bpf_program__attach_usdt(skel->progs.usdt1,
+	skel->links.usdt_sib = bpf_program__attach_usdt(skel->progs.usdt_sib,
 						     0 /*self*/, "/proc/self/exe",
-						     "test", "usdt1", &opts);
-	if (!ASSERT_OK_PTR(skel->links.usdt1, "usdt1_link"))
+						     "test", "usdt_sib", &opts);
+	if (!ASSERT_OK_PTR(skel->links.usdt_sib, "usdt_sib_link"))
 		goto cleanup;
 
 	trigger_func();
 
-	ASSERT_EQ(bss->usdt1_called, 1, "usdt1_called");
-	ASSERT_EQ(bss->usdt1_cookie, 0xcafedeadbeeffeed, "usdt1_cookie");
-	ASSERT_EQ(bss->usdt1_arg_cnt, 1, "usdt1_arg_cnt");
-	ASSERT_EQ(bss->usdt1_arg, test_value, "usdt1_arg");
-	ASSERT_EQ(bss->usdt1_arg_ret, 0, "usdt1_arg_ret");
-	ASSERT_EQ(bss->usdt1_arg_size, sizeof(array[0]), "usdt1_arg_size");
+	ASSERT_EQ(bss->usdt_sib_called, 1, "usdt_sib_called");
+	ASSERT_EQ(bss->usdt_sib_cookie, 0xcafedeadbeeffeed, "usdt_sib_cookie");
+	ASSERT_EQ(bss->usdt_sib_arg_cnt, 1, "usdt_sib_arg_cnt");
+	ASSERT_EQ(bss->usdt_sib_arg, test_value, "usdt_sib_arg");
+	ASSERT_EQ(bss->usdt_sib_arg_ret, 0, "usdt_sib_arg_ret");
+	ASSERT_EQ(bss->usdt_sib_arg_size, sizeof(array[0]), "usdt_sib_arg_size");
+
+	/* auto-attached usdt_rip gets default zero cookie value */
+	ASSERT_EQ(bss->usdt_rip_cookie, 0, "usdt_rip_cookie");
+	ASSERT_EQ(bss->usdt_rip_arg_cnt, 3, "usdt_rip_arg_cnt");
+	ASSERT_EQ(bss->usdt_rip_arg_rets[0], 0, "usdt_rip_arg1_ret");
+	ASSERT_EQ(bss->usdt_rip_arg_rets[1], 0, "usdt_rip_arg2_ret");
+	ASSERT_EQ(bss->usdt_rip_arg_rets[2], 0, "usdt_rip_arg3_ret");
+	ASSERT_EQ(bss->usdt_rip_args[0], c, "usdt_rip_arg1");
+	ASSERT_EQ(bss->usdt_rip_args[1], ti.b, "usdt_rip_arg2");
+	ASSERT_EQ(bss->usdt_rip_args[2], (uintptr_t)add_ptr, "usdt_rip_arg3");
+	ASSERT_EQ(bss->usdt_rip_arg_sizes[0], sizeof(char), "usdt_rip_arg1_size");
+	ASSERT_EQ(bss->usdt_rip_arg_sizes[1], sizeof(long), "usdt_rip_arg2_size");
+	ASSERT_EQ(bss->usdt_rip_arg_sizes[2], sizeof(add_ptr), "usdt_rip_arg3_size");
+
 
 cleanup:
 	test_usdt_o2__destroy(skel);
@@ -67,5 +100,5 @@ cleanup:
 
 void test_usdt_o2(void)
 {
-	basic_sib_usdt();
+	basic_usdt();
 }
--- tools/testing/selftests/bpf/progs/test_usdt_o2.c
+++ tools/testing/selftests/bpf/progs/test_usdt_o2.c
@@ -7,29 +7,64 @@
 
 int my_pid;
 
-int usdt1_called;
-u64 usdt1_cookie;
-int usdt1_arg_cnt;
-int usdt1_arg_ret;
-u64 usdt1_arg;
-int usdt1_arg_size;
+int usdt_sib_called;
+u64 usdt_sib_cookie;
+int usdt_sib_arg_cnt;
+int usdt_sib_arg_ret;
+u64 usdt_sib_arg;
+int usdt_sib_arg_size;
 
 SEC("usdt")
-int usdt1(struct pt_regs *ctx)
+int usdt_sib(struct pt_regs *ctx)
 {
 	long tmp;
 
 	if (my_pid != (bpf_get_current_pid_tgid() >> 32))
 		return 0;
 
-	__sync_fetch_and_add(&usdt1_called, 1);
+	__sync_fetch_and_add(&usdt_sib_called, 1);
 
-	usdt1_cookie = bpf_usdt_cookie(ctx);
-	usdt1_arg_cnt = bpf_usdt_arg_cnt(ctx);
+	usdt_sib_cookie = bpf_usdt_cookie(ctx);
+	usdt_sib_arg_cnt = bpf_usdt_arg_cnt(ctx);
 
-	usdt1_arg_ret = bpf_usdt_arg(ctx, 0, &tmp);
-	usdt1_arg = (u64)tmp;
-	usdt1_arg_size = bpf_usdt_arg_size(ctx, 0);
+	usdt_sib_arg_ret = bpf_usdt_arg(ctx, 0, &tmp);
+	usdt_sib_arg = (u64)tmp;
+	usdt_sib_arg_size = bpf_usdt_arg_size(ctx, 0);
+
+	return 0;
+}
+
+int usdt_rip_called;
+u64 usdt_rip_cookie;
+int usdt_rip_arg_cnt;
+int usdt_rip_arg_rets[3];
+u64 usdt_rip_args[3];
+int usdt_rip_arg_sizes[3];
+
+SEC("usdt//proc/self/exe:test:usdt_rip")
+int usdt_rip(struct pt_regs *ctx)
+{
+	long tmp;
+
+	if (my_pid != (bpf_get_current_pid_tgid() >> 32))
+		return 0;
+
+	__sync_fetch_and_add(&usdt_sib_called, 1);
+
+	usdt_rip_cookie = bpf_usdt_cookie(ctx);
+	usdt_rip_arg_cnt = bpf_usdt_arg_cnt(ctx);
+
+	usdt_rip_arg_rets[0] = bpf_usdt_arg(ctx, 0, &tmp);
+	usdt_rip_args[0] = (char)tmp;
+	usdt_rip_arg_sizes[0] = bpf_usdt_arg_size(ctx, 0);
+
+	usdt_rip_arg_rets[1] = bpf_usdt_arg(ctx, 1, &tmp);
+	usdt_rip_args[1] = (long)tmp;
+	usdt_rip_arg_sizes[1] = bpf_usdt_arg_size(ctx, 1);
+
+	usdt_rip_arg_rets[2] = bpf_usdt_arg(ctx, 2, &tmp);
+	usdt_rip_args[2] = (uintptr_t)tmp;
+	usdt_rip_arg_sizes[2] = bpf_usdt_arg_size(ctx, 2);
 
 	return 0;
 }
